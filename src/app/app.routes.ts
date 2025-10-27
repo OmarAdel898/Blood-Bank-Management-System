f2c01c3abcc063229e72455e91564079ef14040e
hajarhabib2 <hajartech20@gmail.com>
2025-10-27 19:01:15 +0300

    Add register-org feature

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { Home } from './features/home/home';
 import { RegisterDonor } from './features/register-donor/register-donor';
+import { RegisterOrg } from './features/register-org/register-org';
 
 export const routes: Routes = [
   { path: '', component: Home },
   { path: 'register-donor', component: RegisterDonor },
+  { path: 'register-org', component: RegisterOrg },
 ];
