f7186b91ccaa1b44e1c8c3841274c15c2685765f
hajarhabib2 <hajartech20@gmail.com>
2025-10-31 22:52:53 +0200

    add footer feature

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { Navbar } from './shared/components/navbar/navbar';
 import { RouterModule } from '@angular/router';
 import { routes } from './app.routes';
+import { Footer } from './shared/components/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar, RouterModule],
+  imports: [RouterOutlet, Navbar, RouterModule, Footer],
   templateUrl: './app.html',
   styleUrls: ['./app.css'],
 })
