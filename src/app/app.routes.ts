0248b31b0035b21083865c643d0a12b833756f5e
Omar Adel <omaradel.bu@gmail.com>
2025-11-06 01:43:15 +0200

    find blood

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { Home } from './features/home/home';
 import { RegisterDonor } from './features/register-donor/register-donor';
 import { RegisterOrg } from './features/register-org/register-org';
 import { About } from './features/about/about';
+import { BloodBankList } from './features/find-blood/blood-bank-list/blood-bank-list';
 
 export const routes: Routes = [
   { path: '', component: Home },
   { path: 'register-donor', component: RegisterDonor },
   { path: 'register-org', component: RegisterOrg },
   { path: 'about', component: About },
+  { path: 'find-blood', component: BloodBankList },
 ];
