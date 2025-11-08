25a79fea05680b39119cb722a2fe3ba4aec1aff5
hajarhabib2 <hajartech20@gmail.com>
2025-11-09 00:02:30 +0200

    edit footer feature

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,5 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 import { Home } from './features/home/home';
 import { RegisterDonor } from './features/register-donor/register-donor';
 import { RegisterOrg } from './features/register-org/register-org';
@@ -9,4 +10,16 @@ export const routes: Routes = [
   { path: 'register-donor', component: RegisterDonor },
   { path: 'register-org', component: RegisterOrg },
   { path: 'about', component: About },
+  { path: '**', redirectTo: '' },
 ];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, {
+      scrollPositionRestoration: 'top', // 👈✨ السطر المهم هنا
+      anchorScrolling: 'enabled', // (اختياري) لو عندك روابط داخل الصفحة نفسها #
+    }),
+  ],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,16 +1,25 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, signal, inject } from '@angular/core';
+import { Router, RouterOutlet, NavigationEnd } from '@angular/router';
 import { Navbar } from './shared/components/navbar/navbar';
 import { RouterModule } from '@angular/router';
 import { routes } from './app.routes';
 import { Footer } from './shared/components/footer/footer';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, Navbar, RouterModule, Footer],
+  standalone: true,
   templateUrl: './app.html',
   styleUrls: ['./app.css'],
 })
 export class App {
   protected readonly title = signal('blood-bank');
+  private readonly router = inject(Router);
+
+  constructor() {
+    this.router.events.pipe(filter((event) => event instanceof NavigationEnd)).subscribe(() => {
+      window.scrollTo({ top: 0, behavior: 'smooth' });
+    });
+  }
 }
